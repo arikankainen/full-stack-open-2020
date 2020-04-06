206a8118f5f6616daa384bc0c3db8596aab3bc3a
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-04-06 18:22:41 +0300

    1.8: unicafe step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,22 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button name='good' handleClick={() => setGood(good + 1)} />
-      <Button name='neutral' handleClick={() => setNeutral(neutral + 1)} />
-      <Button name='bad' handleClick={() => setBad(bad + 1)} />
-      <Stats good={good} neutral={neutral} bad={bad} />
-    </div>
-  )
-}
-
 const Button = ({ name, handleClick}) => {
   return (
     <>
@@ -27,7 +11,7 @@ const Button = ({ name, handleClick}) => {
   )
 }
 
-const Stats = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
   
   let average = (good + -bad) / all
@@ -45,6 +29,22 @@ const Stats = ({ good, neutral, bad }) => {
       <p>all {all}</p>
       <p>average {average}</p>
       <p>positive {positive} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button name='good' handleClick={() => setGood(good + 1)} />
+      <Button name='neutral' handleClick={() => setNeutral(neutral + 1)} />
+      <Button name='bad' handleClick={() => setBad(bad + 1)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
