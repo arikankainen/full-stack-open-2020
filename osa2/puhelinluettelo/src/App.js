677e120cdcf3bda42d3c7b88f8b016b517241aba
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-04-09 16:46:19 +0300

    2.10: puhelinluettelo step5

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -56,18 +56,42 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input value={filter} onChange={handleFilter} /></div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>name: <input value={newName} onChange={handleNewName} /></div>
-        <div>number: <input value={newNumber} onChange={handleNewNumber} /></div>
-        <div><button type="submit">add</button></div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map((person) => <li key={person.name}>{person.name} {person.number}</li>)}
-      </ul>
+      <Filter filter={filter} handleChange={handleFilter} />
+      <h3>add a new</h3>
+      <PersonForm handleSubmit={addPerson} name={newName} handleNameChange={handleNewName} number={newNumber} handleNumberChange={handleNewNumber} />
+      <h3>Numbers</h3>
+      <Numbers persons={personsToShow} />
     </div>
+  )
+}
+
+const Filter = ({filter, handleChange}) => {
+  return (
+    <div>filter shown with <input value={filter} onChange={handleChange} /></div>
+  )
+}
+
+const PersonForm = ({handleSubmit, name, handleNameChange, number, handleNumberChange}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>name: <input value={name} onChange={handleNameChange} /></div>
+      <div>number: <input value={number} onChange={handleNumberChange} /></div>
+      <div><button type="submit">add</button></div>
+    </form>
+  )
+}
+
+const Numbers = ({persons}) => {
+  return (
+    <ul>
+      {persons.map((person) => <Number key={person.name} person={person} />)}
+    </ul>
+  )
+}
+
+const Number = ({person}) => {
+  return (
+    <li>{person.name} {person.number}</li>
   )
 }
 
